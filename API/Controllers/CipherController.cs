ecdb6464bc04c52db046ec68579fab021029fd18
MischaTobias <mischa.tobias4b@gmail.com>
2020-10-28 00:07:33 -0600

    Created Project

---
+++ API/Controllers/CipherController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CipherController : ControllerBase
+    {
+        // GET: api/<CipherController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CipherController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CipherController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CipherController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CipherController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Encryptions/Interfaces/IEncryptor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encryptions.Interfaces
+{
+    interface IEncryptor<T>
+    {
+        string EncryptFile(string completeFilePath, T key);
+        string DecryptFile(string completeFilePath, T key);
+        string EncryptString(string text, T Key);
+        string DecryptString(string text, T Key);
+    }
+}
